e779ce52267c2bde9aa62fb0471de827bcbf0f7d
Chris Neale <chris@usablehq.com>
2020-11-02 07:50:24 +0000

    Added land

--- src/InstancedApp.js
+++ src/InstancedApp.js
@@ -8,17 +8,19 @@ import { OrbitControls } from '@react-three/drei/OrbitControls'
 
 import './App.css'
 
-const dimX = 150;
-const dimY = 150;
+const dimX = 200;
+const dimY = 200;
 const cubeCount = dimX * dimY;
 
 const tempObject = new THREE.Object3D()
 const tempColor = new THREE.Color()
-const colorPalette = ['#4444ff', '#3333ff', '#2222ff', '#1111ff', '#0000ff']
-const colors = new Array(cubeCount).fill().map(() => colorPalette[Math.floor(Math.random() * 5)])
 
 const noiseFunc = new tumult.Perlin2()
-const noise = new Array(cubeCount).fill().map((value, i) => noiseFunc.gen(Math.floor(i / dimX) / 20, Math.floor(i % dimY) / 20))
+const noise = new Array(cubeCount).fill().map((_, i) => (1 + noiseFunc.octavate(1.25, Math.floor(i / dimX) / 20, Math.floor(i % dimY) / 20)) * 0.5)
+
+const colorPalette = ['#1111ff', '#1111ff', '#1111ff', '#1111ff', '#1111ff', '#fcd046', '#00aa00', '#007700', '#004400']
+const colors = new Array(cubeCount).fill().map((_, i) => colorPalette[Math.floor(noise[i] * 9)])
+const speeds = new Array(cubeCount).fill().map((_, i) => Math.floor(noise[i] * 9) < 5 ? 1 : 0)
 
 function Boxes() {
   const colorArray = useMemo(() => Float32Array.from(new Array(cubeCount).fill().flatMap((_, i) => tempColor.set(colors[i]).toArray())), [])
@@ -34,8 +36,8 @@ function Boxes() {
     for (let x = 0; x < dimX; x++) {
       for (let z = 0; z < dimY; z++) {
         const id = i++
-        tempObject.position.set(dimX/2 - x, 0, dimY/2 - z)
-        tempObject.position.y = Math.sin(x / 4 + time) +(noise[x + z * dimY]) * 2
+        tempObject.position.set(dimX/2 - x, (-3 + (7 * noise[x * dimY + z])) * (1 - speeds[x * dimY + z]), dimY/2 - z)
+        tempObject.position.y += ((Math.sin(x / 4 + time) + Math.sin(0 / 4 + time) + Math.sin(z / 4 + time)) * 0.5 + (noise[x + z * dimY])) * speeds[x * dimY + z]
         // tempObject.rotation.y = Math.sin(x / 4 + time) + Math.sin(0 / 4 + time) + Math.sin(z / 4 + time)
         // tempObject.rotation.z = tempObject.rotation.y * 2
         tempObject.updateMatrix()
@@ -47,7 +49,7 @@ function Boxes() {
 
   return (
     <instancedMesh ref={ref} args={[null, null, cubeCount]}>
-      <boxBufferGeometry attach="geometry" args={[1.0, 1.0, 1.0]}>
+      <boxBufferGeometry attach="geometry" args={[1.0, 2.0, 1.0]}>
         <instancedBufferAttribute attachObject={['attributes', 'color']} args={[colorArray, 3]} />
       </boxBufferGeometry>
       <meshPhongMaterial attach="material" vertexColors={THREE.VertexColors} />
